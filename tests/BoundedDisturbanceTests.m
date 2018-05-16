edc6b3730dff711098f0d8f76a8a50d4d9063ced
Joseph Gleason <gleasonj@unm.edu>
2018-05-16 15:25:23 -0600

    More updates to class-based unit testing
    Updated:
        - StochasticDisturbanceTests
        - BoundedDisturbanceTests
        - Documentation updates

---
+++ tests/BoundedDisturbanceTests.m
@@ -0,0 +1,89 @@
+classdef BoundedDisturbanceTests < matlab.unittest.TestCase
+% Socbox/BoundedDisturbanceTests: Unit tests for bounded disturbances
+% ===========================================================================
+%
+% Unit testing for bounded disturbances
+%
+% Usage:
+% ------
+% tests = BoundedDisturbanceTests()
+% run(tests)
+%
+% ===========================================================================
+%
+% This function is part of the Stochastic Optimal Control Toolbox.
+% License for the use of this function is given in
+%      https://github.com/abyvinod/Socbox/blob/master/LICENSE
+%
+%
+
+    methods (Test)
+        function testBoundedEllipseRandomDirections(test_case)
+        % Socbox/BoundedDisturbanceTests/testBoundedEllipseRandomDirections: 
+        % Unit test for getBoundedSetForDisturbance for random ellipsoid 
+        % generation
+        % ====================================================================
+        %
+        % Unit test for getBoundedSetForDisturbance using ellipsoid 
+        % overapproximation via random direction selection
+        %   
+        % ====================================================================
+        %
+        % This function is part of the Stochastic Optimal Control Toolbox.
+        % License for the use of this function is given in
+        %      https://github.com/abyvinod/Socbox/blob/master/LICENSE
+        %
+        %   
+
+            T = 0.25;
+            umax = 0.75;
+            dmax = 0.097;
+
+            sys = LtiSystem(...
+                'StateMatrix', [1, T; 0, 1], ...
+                'InputMatrix', [T^2; T], ...
+                'InputSpace', Polyhedron('lb', -umax, 'ub', umax), ...
+                'DisturbanceMatrix', eye(2), ...
+                'Disturbance', StochasticDisturbance('Gaussian', ...
+                    zeros(2,1), 0.01* eye(2)));
+
+            beta = 0.7;
+            time_horizon = 5;
+
+            bounded_dist = getBoundedSetForDisturbance(...
+                sys.disturbance, ...
+                time_horizon, ...
+                beta, ...
+                'random', ...
+                100);
+
+            test_case.verifyInstanceOf(bounded_dist, 'Polyhedron');
+        end
+
+        function testLoadBoundedDisturbance(test_case)
+        % Socbox/BoundedDisturbanceTests/testBoundedEllipseRandomDirections: 
+        % Unit test for getBoundedSetForDisturbance for random ellipsoid 
+        % generation
+        % ====================================================================
+        %
+        % Unit test for getBoundedSetForDisturbance using load from file method
+        %   
+        % ====================================================================
+        %
+        % This function is part of the Stochastic Optimal Control Toolbox.
+        % License for the use of this function is given in
+        %      https://github.com/abyvinod/Socbox/blob/master/LICENSE
+        %
+        %
+
+            bounded_dist = getBoundedSetForDisturbance(...
+                [],...
+                [],...
+                [],... 
+                'load', ...
+                'data/cwhUnderapproxBoundeSet.mat');
+
+            test_case.verifyInstanceOf(bounded_dist, 'Polyhedron');
+        end
+    end
+end
