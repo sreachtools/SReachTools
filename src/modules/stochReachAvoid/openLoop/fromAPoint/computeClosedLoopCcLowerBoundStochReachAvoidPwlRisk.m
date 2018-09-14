7e093eaa46e709d298577302a88263e35137ee9f
Abraham <aby.vinod@gmail.com>
2018-09-14 01:36:08 -0600

    Closed loop controller generation is working for a fixed Delta_x Delta_u #53

--- src/modules/stochReachAvoid/openLoop/fromAPoint/computeClosedLoopCcLowerBoundStochReachAvoidPwlRisk.m
+++ src/modules/stochReachAvoid/openLoop/fromAPoint/computeClosedLoopCcLowerBoundStochReachAvoidPwlRisk.m
@@ -1,4 +1,4 @@
-function [lb_stoch_reach_avoid, optimal_input_vector, optimal_input_gain] =...
+function [lb_stoch_reach_avoid, optimal_input_vector, optimal_input_gain, input_violation_prob] =...
     computeClosedLoopCcLowerBoundStochReachAvoidPwlRisk( ...
         sys,...
         time_horizon,...
@@ -9,7 +9,6 @@ function [lb_stoch_reach_avoid, optimal_input_vector, optimal_input_gain] =...
         H, ...
         G, ...
         mean_X_sans_input, ...
-        cov_X_sans_input, ...
         cov_concat_disturb, ...
         desired_accuracy)
 % Solve the stochastic reach-avoid problem (lower bound on the probability and
@@ -110,14 +109,16 @@ function [lb_stoch_reach_avoid, optimal_input_vector, optimal_input_gain] =...
     %% Compute M --- the number of polytopic halfspaces to worry about
     n_lin_state = size(concat_target_tube_A,1);
     n_lin_input = size(concat_input_space_A,1);
-
+    
+    
     %% Obtain the piecewise linear overapproximation of norminvcdf in the
     %interval [0,0.44]
     [invcdf_approx_m, invcdf_approx_c, lb_deltai,useful_knots] =...
         computeNormCdfInvOverApprox(0.44,...
             5e-3,...
             n_lin_state);
-
+    n_pwa = length(invcdf_approx_m);
+    
     %% Solve the feasibility problem
 %     cvx_begin quiet
 %         variable U_vector(sys.input_dim * time_horizon,1);
@@ -138,81 +139,142 @@ function [lb_stoch_reach_avoid, optimal_input_vector, optimal_input_gain] =...
 %             deltai <= 0.5;
 %             sum(deltai) <= 0.5;
 %     cvx_end
-    Delta_input = 1;
-    Delta_state = 0.2;
-    d_vector = sdpvar(sys.input_dim * time_horizon, 1);
-    mean_X = sdpvar(sys.state_dim * time_horizon, 1);
-    
-    n_pwa = length(invcdf_approx_m);
-    
-    deltai = sdpvar(n_lin_state, 1);
-    sigma_vector_state = sdpvar(n_lin_state, 1);
-    sigma_vector_state_slack = sdpvar(n_lin_state, 1);
-    slack_cc_state = sdpvar(n_lin_state, n_pwa, 'full');
-    
-    gammai = sdpvar(n_lin_input, 1);
-    sigma_vector_input = sdpvar(n_lin_input, 1);
-    sigma_vector_input_slack = sdpvar(n_lin_input, 1);
-    slack_cc_input = sdpvar(n_lin_input, n_pwa, 'full');
-    
-    % M_matrix is a decision variable but it is typically buried inside
-    % norms
-    M_matrix = sdpvar(sys.input_dim * time_horizon, sys.dist_dim * time_horizon, 'full');
-    sqrt_cov_state_matrix = sdpvar(sys.state_dim * time_horizon, sys.dist_dim * time_horizon);
-    sqrt_cov_state_matrix = (H * M_matrix + G) * chol(cov_concat_disturb);
-    sqrt_cov_input_matrix = sdpvar(sys.input_dim * time_horizon, sys.dist_dim * time_horizon);
-    sqrt_cov_input_matrix = M_matrix * chol(cov_concat_disturb);
-    
-    objective = sum(sigma_vector_input_slack) + sum(sigma_vector_state_slack);%sum(deltai);%sum(sigma_vector_state) + sum(sigma_vector_input);
-
-    for time_indx = 1:time_horizon - 1
-        M_matrix((time_indx-1)*sys.input_dim + 1:time_indx*sys.input_dim, (time_indx)*sys.dist_dim+1:end) =0; 
-    end
-    
-    constraints = [(mean_X == mean_X_sans_input + H * d_vector):'Mean trajectory constraint',...
-                   (0 <= slack_cc_input):'Positive slack variables input',...
-                   (0 <= slack_cc_state):'Positive slack variables state',...
-                   (sigma_vector_input <= sigma_vector_input_slack):'Slack variable for convexity of norms input'
-                   (sigma_vector_state <= sigma_vector_state_slack):'Slack variable for convexity of norms state'
-                   (lb_deltai <= gammai <= 0.43):'Risk allocation in [lb_deltai,0.43] input',...  %0.43 since we can't go all the way to 0.5
-                   (lb_deltai <= deltai <= 0.43):'Risk allocation in [lb_deltai,0.43] state',...
-                   (sum(deltai) <= Delta_state):'Max tolerance in state',...   %TODO: Is it 0.5 or 1 or necessary?
-                   (sum(gammai) <= Delta_input):'Max tolerance in input'];
-    
-   for approx_indx = 1:length(invcdf_approx_m)
-        positive_m_value = abs(invcdf_approx_m(approx_indx));
-        positive_c_value = invcdf_approx_c(approx_indx);
-        constraints = [constraints,...
-                       (concat_input_space_A * d_vector - concat_input_space_b + sigma_vector_input * positive_c_value <= slack_cc_input(:,approx_indx)):['LHS-CC input for each PWA ' num2str(approx_indx)],...
-                       (concat_target_tube_A * mean_X   - concat_target_tube_b + sigma_vector_state * positive_c_value <= slack_cc_state(:,approx_indx)):['LHS-CC state for each PWA ' num2str(approx_indx)]];
-        for state_indx = 1:n_lin_state
-            sigma_vector_state(state_indx) = norm(concat_target_tube_A(state_indx,:)*sqrt_cov_state_matrix);
-            constraints = [constraints,...
-                           (slack_cc_state(state_indx,approx_indx) <= positive_m_value * sigma_vector_state_slack(state_indx) * deltai(state_indx)):['RHS-CC state for each PWA ' num2str(approx_indx) ', ' num2str(state_indx)]];                       
-        end
-        
-        for input_indx = 1:n_lin_input
-            sigma_vector_input(input_indx) = norm(concat_input_space_A(input_indx,:)*sqrt_cov_input_matrix);
-            constraints = [constraints,...
-                           (slack_cc_input(input_indx,approx_indx) <= positive_m_value * sigma_vector_input_slack(input_indx) * gammai(input_indx)):['RHS-CC input for each PWA ' num2str(approx_indx) ', ' num2str(input_indx)]];                       
-        end
-    end
-               
-    % Set some options for YALMIP and solver
-    options = sdpsettings('verbose',2);
-    
-    constraints
-    
-    % Solve the problem
-    sol = optimize(constraints,objective,options);
-
-    % Analyze error flags
-    if sol.problem == 0
-        % Extract and display value
-        lb_stoch_reach_avoid = 1-sum(value(deltai));
-        optimal_input_vector = value(d_vector);
-        optimal_input_gain = value(M_matrix);
+    if exist('ccc-solver.mat','file')
+        load('ccc-solver','ccc_closed_solver');
     else
-        sol
+        Delta_input = sdpvar(1);
+        Delta_state = sdpvar(1);
+        d_vector = sdpvar(sys.input_dim * time_horizon, 1);
+        mean_X = sdpvar(sys.state_dim * time_horizon, 1);
+
+        deltai = sdpvar(n_lin_state, 1);
+        sigma_vector_state = sdpvar(n_lin_state, 1);
+        sigma_vector_state_replace_slack = sdpvar(n_lin_state, 1);
+        slack_cc_state = sdpvar(n_lin_state, n_pwa, 'full');
+        slack_cc_sqrt_state = sdpvar(n_lin_state, n_pwa, 'full');
+        slack_cc_sqrt_state_iter = sdpvar(n_lin_state, n_pwa, 'full');
+        slack_reverse_state = sdpvar(n_lin_state, n_pwa, 'full');
+
+        gammai = sdpvar(n_lin_input, 1);
+        sigma_vector_input = sdpvar(n_lin_input, 1);
+        sigma_vector_input_replace_slack = sdpvar(n_lin_input, 1);
+        slack_cc_input = sdpvar(n_lin_input, n_pwa, 'full');
+        slack_cc_sqrt_input = sdpvar(n_lin_input, n_pwa, 'full');
+        slack_cc_sqrt_input_iter = sdpvar(n_lin_input, n_pwa, 'full');
+        slack_reverse_input = sdpvar(n_lin_input, n_pwa, 'full');
+
+        % M_matrix is a decision variable but it is typically buried inside
+        % norms
+        M_matrix = sdpvar(sys.input_dim * time_horizon, sys.dist_dim * time_horizon, 'full');
+        sqrt_cov_state_matrix = (H * M_matrix + G) * chol(cov_concat_disturb);
+        sqrt_cov_input_matrix = M_matrix * chol(cov_concat_disturb);
+
+        for time_indx = 1:time_horizon - 1
+            M_matrix((time_indx-1)*sys.input_dim + 1:time_indx*sys.input_dim, (time_indx-1)*sys.dist_dim+1:end) =0; 
+        end
+
+
+        constraints = [(mean_X == mean_X_sans_input + H * d_vector):'Mean trajectory constraint',...
+                       (0 <= slack_cc_input):'Positive slack variables CC input',...
+                       (0 <= slack_cc_state):'Positive slack variables CC state',...
+                       (0 <= slack_cc_sqrt_input):'Positive slack variables CC-sqrt input',...
+                       (0 <= slack_cc_sqrt_state):'Positive slack variables CC-sqrt state',...
+                       (sigma_vector_input <= sigma_vector_input_replace_slack):'Slack variable for convexity of norms input'
+                       (sigma_vector_state <= sigma_vector_state_replace_slack):'Slack variable for convexity of norms state'
+                       (lb_deltai <= gammai <= 0.43):'Risk allocation in [lb_deltai,0.43] input',...  %0.43 since we can't go all the way to 0.5
+                       (lb_deltai <= deltai <= 0.43):'Risk allocation in [lb_deltai,0.43] state',...
+                       (sum(deltai) <= Delta_state):'Max tolerance in state',...   %TODO: Is it 0.5 or 1 or necessary?
+                       (sum(gammai) <= Delta_input):'Max tolerance in input',...
+                       (slack_reverse_state >= 0):'Reverse cvx slack variable in input',... % Reverse convex inequality of slack_cc_sqrt_X^2 - slack_cc_X >= 0                   
+                       (slack_reverse_input >= 0):'Reverse cvx slack variable in state',... 
+                       (slack_cc_state - slack_cc_sqrt_state_iter.^2 - 2 * slack_cc_sqrt_state_iter.*(slack_cc_sqrt_state - slack_cc_sqrt_state_iter) <= slack_reverse_state):'Connecting slack variables for state',...
+                       (slack_cc_input - slack_cc_sqrt_input_iter.^2 - 2 * slack_cc_sqrt_input_iter.*(slack_cc_sqrt_input - slack_cc_sqrt_input_iter) <= slack_reverse_input):'Connecting slack variables for input'];
+
+       for approx_indx = 1:length(invcdf_approx_m)
+            positive_m_value = abs(invcdf_approx_m(approx_indx));
+            positive_c_value = invcdf_approx_c(approx_indx);
+            constraints = [constraints,...
+                           (concat_input_space_A * d_vector - concat_input_space_b + sigma_vector_input * positive_c_value <= slack_cc_input(:,approx_indx)):['LHS-CC input for each PWA ' num2str(approx_indx)],...
+                           (concat_target_tube_A * mean_X   - concat_target_tube_b + sigma_vector_state * positive_c_value <= slack_cc_state(:,approx_indx)):['LHS-CC state for each PWA ' num2str(approx_indx)]];
+            for state_indx = 1:n_lin_state
+                sigma_vector_state(state_indx) = norm(concat_target_tube_A(state_indx,:)*sqrt_cov_state_matrix);
+                constraints = [constraints,...
+                               (norm([2*slack_cc_sqrt_state(state_indx,approx_indx);
+                                      sigma_vector_state_replace_slack(state_indx) - positive_m_value * deltai(state_indx)]) <= sigma_vector_state_replace_slack(state_indx) + positive_m_value * deltai(state_indx)):['RHS-CC hyperbolic state for each PWA ' num2str(approx_indx) ', ' num2str(state_indx)]];
+            end
+
+            for input_indx = 1:n_lin_input
+                sigma_vector_input(input_indx) = norm(concat_input_space_A(input_indx,:)*sqrt_cov_input_matrix);
+                constraints = [constraints,...
+                               (norm([2*slack_cc_sqrt_input(input_indx,approx_indx);
+                                      sigma_vector_input_replace_slack(input_indx) - positive_m_value * gammai(input_indx)]) <= sigma_vector_input_replace_slack(input_indx) + positive_m_value * gammai(input_indx)):['RHS-CC hyperbolic input for each PWA ' num2str(approx_indx) ', ' num2str(input_indx)]];
+            end        
+        end
+
+        % Set some options for YALMIP and solver
+        options = sdpsettings('verbose',0,'solver','gurobi');
+
+        objective = sum(sigma_vector_input_replace_slack) + sum(sigma_vector_state_replace_slack);
+
+        constraints
+
+        tau = sdpvar(1);
+        dc_slack_with_tau = tau * (sum(sum(slack_reverse_state))+sum(sum(slack_reverse_input)));
+
+        ccc_closed_solver = optimizer([constraints],...
+                                      objective + dc_slack_with_tau,options,...
+                                      {slack_cc_sqrt_state_iter, slack_cc_sqrt_input_iter, tau, Delta_state, Delta_input},...
+                                      {slack_cc_sqrt_state, slack_cc_sqrt_input, objective, dc_slack_with_tau,...
+                                       deltai, gammai, d_vector, M_matrix, slack_cc_state, slack_cc_input});
+%         save('ccc-solver','ccc_closed_solver');
     end
+    %% Difference of convex penalty approach
+    tau_initial = 1;
+    scaling_tau = 2;
+    tau_max = 1e5;
+    iter_max = 1e3;
+    iter_count = 1;
+    obj_prev = Inf;
+    dc_slack_with_tau_prev = Inf;
+    dc_conv_tol = 1e-4;
+    delta_solve_state = 0.1;
+    delta_solve_input = 0.1;
+    
+    tau_iter = tau_initial;
+    
+    delta_solve = [delta_solve_state, delta_solve_input];
+    sol = ccc_closed_solver(ones(n_lin_state, n_pwa), ones(n_lin_input, n_pwa), tau_iter, delta_solve(1), delta_solve(2));
+    obj_curr = sol{3};
+    dc_slack_with_tau_curr = sol{4};
+    tau_iter = min(tau_iter * scaling_tau, tau_max);
+    
+    
+    if isnan(obj_curr)
+        disp('Oops! Initial guess didn''t work')
+    else
+        while abs((obj_prev + dc_slack_with_tau_prev)- obj_curr + dc_slack_with_tau_curr) >= dc_conv_tol && iter_count < iter_max
+            obj_prev = obj_curr;
+            dc_slack_with_tau_prev = dc_slack_with_tau_curr;
+            
+            sol = ccc_closed_solver(sol{1},sol{2}, tau_iter, delta_solve(1), delta_solve(2));            
+            if isnan(obj_curr)
+                disp('Converged to a infeasible point!');
+            else
+                obj_curr = sol{3};
+                dc_slack_with_tau_curr = sol{4};
+                slack_cc_state_curr = sol{9};
+                slack_cc_input_curr = sol{10};
+                max_slack_sqrt_error = max(max(max(sol{1}.^2 - slack_cc_state_curr)),max(max(sol{2}.^2 - slack_cc_input_curr)));
+                exit_condition = abs(obj_curr + dc_slack_with_tau_curr - obj_prev - dc_slack_with_tau_prev);
+                fprintf('%d. Total error: %1.2e | tau_iter: %6d | Curr-Prev --- DC: %1.2e ; Obj: %1.2e | curr DC const: \n',iter_count,exit_condition, tau_iter, dc_slack_with_tau_prev - dc_slack_with_tau_curr, obj_prev - obj_curr, max_slack_sqrt_error);
+                tau_iter = min(tau_iter * scaling_tau, tau_max);
+                iter_count = iter_count + 1;
+            end
+        end
+    end    
+    % Extract and display value
+    lb_stoch_reach_avoid = 1-sum(sol{5});
+    input_violation_prob = 1-sum(sol{6});
+    optimal_input_vector = sol{7};
+    optimal_input_gain = sol{8};
 end
