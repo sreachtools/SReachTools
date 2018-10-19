dc04a466ab900171812f1b9d21d20a5f983014db
Abraham <aby.vinod@gmail.com>
2018-10-19 10:04:20 -0600

    Fix tests

--- Contents.m
+++ Contents.m
@@ -1,19 +1,31 @@
 % Stochastic Reachability Toolbox (SReachTools)
-% Version 0.2 (R2017a) 31-May-2018
+% Version 0.3.5 (R2017b) 19-October-2018
 % 
 % The Stochastic Reachability Toolbox (SReachTools) is an open-source MATLAB 
 % toolbox for performing stochastic verification and reachability analysis.
 %
-% For questions and comments visit the project's website at
+% For questions and comments, visit the project's website at
 %
 %    https://unm-hscl.github.io/SReachTools/
 %
-% Basic Functionality:
-%     - Lagrangian methods for computing approximations of the stochastic 
-%       reach-avoid set
-%     - Fourier transform methods for fast, high-dimensional computation of
-%       reach-avoid sets using open-loop controllers
-%     - Dynamic programming methods for 'exact' computation of the reach-avoid
+% Basic Functionality: Compute the set of safe initial states and associated
+%   admissible controllers to drive the system to stay with in a target tube (a
+%   time-varying collection of target tubes)
+%     - Stochastic reach set (approximations) via 
+%           1. Lagrangian methods
+%           2. Convex chance constrained optimization
+%           3. Fourier transform (Genz's algorithm and MATLAB's patternsearch)
+%     - Optimal controller synthesis
+%           1. Open-loop controller synthesis
+%               1. Convex chance constrained optimization
+%               2. Fourier transform (Genz's algorithm and MATLAB's
+%                  patternsearch)
+%               3. Particle filter
+%           1. Affine controller synthesis
+%               1. Difference of convex and chance constrained optimization
+%     - Dynamic programming methods for 'exact' computation of the reach
 %       probability using backward recursion
+%     - Forward stochastic reachability analysis via Gaussian random vector
+%       properties
 %     
 
